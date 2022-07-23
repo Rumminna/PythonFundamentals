1015f1b754e0fc44f9d165003ccdd310433dcea9
Rumyana Petrova <67527127+Rumminna@users.noreply.github.com>
2022-07-23 23:26:52 +0200

    Update 07.Faro_Shuffle.py

--- 3.List/07.Faro_Shuffle.py
+++ 3.List/07.Faro_Shuffle.py
@@ -1,14 +1,37 @@
-deck = input().split(" ")
-number_of_shuffles = int(input())
+deck = input().split()
+shuffles_count = int(input())
+
 left_half = []
 right_half = []
 
-for shuffles in range(number_of_shuffles):
-    current_deck = []
-    half = int(len(deck))
-    left_half = deck[0:half]
-    right_half = deck[half::]
+for shuffle in range(shuffles_count):
 
-    for card in range(len(left_half)):
+    current_deck = []
+
+    half = len(deck)//2
+    left_half = deck[:half]
+    right_half = deck[half:]
+
+    for card in range(half):
+
         current_deck.append(left_half[card])
         current_deck.append(right_half[card])
+
+    deck = current_deck.copy()
+
+print(deck)
+
+
+# cards = input().split()
+# shuffles_count = int(input())
+# middle = len(cards)//2
+
+# for _ in range(shuffles_count):
+
+#     current_shuffle = zip(cards[:middle], cards[middle:])
+
+#     cards.clear()
+#     for pair in current_shuffle:
+#         cards.extend(pair)
+
+# print(cards)
